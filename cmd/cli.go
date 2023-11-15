b62097f0df3f2c8ab703ad67a7c7c97c4c148967
Micah Walter <47419+micahwalter@users.noreply.github.com>
2023-11-15 02:52:49 +0000

    added test program

---
+++ cmd/cli.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/go-micah/go-bedrock"
+)
+
+func main() {
+	
+	prompt := "Please write me a short poem about a chicken"
+
+	var options bedrock.Options
+
+	options.ModelID = "anthropic.claude-v2"
+	options.Region = "us-east-1"
+
+	options.MaxTokensToSample = 500
+
+	options.Temperature = 1
+	options.TopP = 0.999
+	options.TopK = 250
+
+	options.StopSequences = []string{`"\n\nHuman:\"`}
+
+	resp, err := bedrock.SendToBedrock(prompt, options)
+	if err != nil {
+		fmt.Printf("error: %v", err)
+	}
+
+	var response bedrock.AnthropicResponse
+
+	err = json.Unmarshal(resp.Body, &response)
+
+	if err != nil {
+		log.Fatal("failed to unmarshal", err)
+	}
+
+	fmt.Print(response.Completion)
+}
