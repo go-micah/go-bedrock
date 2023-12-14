6f6aabd9d9cc7a3f74f699a3d6c781ce7bc14f16
Micah Walter <micawal@amazon.com>
2023-12-14 10:09:27 -0500

    upgrading deps

--- cmd/cli.go
+++ cmd/cli.go
@@ -3,8 +3,6 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
-	"time"
 
 	"github.com/go-micah/go-bedrock"
 )
@@ -37,110 +35,5 @@ func main() {
 		log.Fatal("error", err)
 	}
 	fmt.Println(text)
-
-	// prepare payload for AI21 Labs Jurassic 2
-	jurassic := bedrock.AI21LabsJurassic{
-		Region:            "us-east-1",
-		ModelId:           "ai21.j2-mid-v1",
-		PromptRequest:     prompt,
-		MaxTokensToSample: 200,
-		TopP:              1,
-		Temperature:       0.7,
-		StopSequences:     []string{`""`},
-	}
-
-	fmt.Println("Sending prompt to AI21 Jurassic 2")
-	
-	resp, err = jurassic.InvokeModel()
-	if err != nil {
-		log.Fatal("error", err)
-	}
-
-	text, err = jurassic.GetText(resp)
-	if err != nil {
-		log.Fatal("error", err)
-	}
-	fmt.Println(text)
-
-	// prepare payload for Meta Llama
-	llama := bedrock.MetaLlama{
-		Region:            "us-east-1",
-		ModelId:           "meta.llama2-13b-chat-v1",
-		Prompt:            prompt,
-		MaxTokensToSample: 512,
-		TopP:              0.9,
-		Temperature:       0.5,
-	}
-
-	fmt.Println("Sending prompt to Meta Llama")
-
-	resp, err = llama.InvokeModel()
-	if err != nil {
-		log.Fatal("error", err)
-	}
-
-	text, err = llama.GetText(resp)
-	if err != nil {
-		log.Fatal("error", err)
-	}
-	fmt.Println(text)
-
-	// prepare payload for Cohere Command v14
-	command := bedrock.CohereCommand{
-		Region:            "us-east-1",
-		ModelId:           "cohere.command-text-v14",
-		Prompt:            prompt,
-		Temperature:       0.75,
-		TopP:              0.01,
-		TopK:              0,
-		MaxTokensToSample: 400,
-		StopSequences:     []string{`""`},
-		ReturnLiklihoods:  "NONE",
-		NumGenerations:    1,
-	}
-
-	fmt.Println("Sending prompt to Cohere Command")
-
-	resp, err = command.InvokeModel()
-	if err != nil {
-		log.Fatal("error", err)
-	}
-
-	text, err = command.GetText(resp)
-	if err != nil {
-		log.Fatal("error", err)
-	}
-	fmt.Println(text)
-
-	// prepare payload for Stability SD
-
-	stability := bedrock.StabilityAISD{
-		Region:  "us-east-1",
-		ModelId: "stability.stable-diffusion-xl-v0",
-		Prompt:  []bedrock.StabilityAISDTextPrompts{{Text: "A cat driving a convertible along the coast"}},
-		Scale:   10,
-		Seed:    0,
-		Steps:   50,
-	}
-
-	fmt.Println("Sending prompt to Stability SD")
-
-	resp, err = stability.InvokeModel()
-	if err != nil {
-		log.Fatal("error", err)
-	}
-
-	image, err := stability.GetDecodedImage(resp)
-	if err != nil {
-		log.Fatal("error", err)
-	}
-	outputFile := fmt.Sprintf("output-%d.jpg", time.Now().Unix())
-
-	err = os.WriteFile(outputFile, image, 0644)
-	if err != nil {
-		fmt.Println("error writing to file:", err)
-	}
-
-	log.Println("image written to file", outputFile)
 
 }
--- go.mod
+++ go.mod
@@ -1,23 +1,24 @@
 module github.com/go-micah/go-bedrock
 
-go 1.21.3
+go 1.21.5
 
 require (
-	github.com/aws/aws-sdk-go-v2/config v1.25.0
-	github.com/aws/aws-sdk-go-v2/service/bedrockruntime v1.3.1
+	github.com/aws/aws-sdk-go-v2/config v1.26.1
+	github.com/aws/aws-sdk-go-v2/service/bedrockruntime v1.5.5
 )
 
 require (
 	github.com/aws/aws-sdk-go-v2 v1.24.0 // indirect
-	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.5.0 // indirect
-	github.com/aws/aws-sdk-go-v2/credentials v1.16.0 // indirect
-	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.14.3 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/configsources v1.2.2 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.5.2 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/ini v1.7.0 // indirect
-	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.10.2 // indirect
-	github.com/aws/aws-sdk-go-v2/service/sso v1.17.1 // indirect
-	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.19.1 // indirect
-	github.com/aws/aws-sdk-go-v2/service/sts v1.25.1 // indirect
+	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.5.4 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.16.12 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.14.10 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.2.9 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.5.9 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.7.2 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.10.4 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.10.9 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.18.5 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.21.5 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.26.5 // indirect
 	github.com/aws/smithy-go v1.19.0 // indirect
 )
