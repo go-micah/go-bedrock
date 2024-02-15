48da1046df69a67e94095b4b99478d16b0cdbf3f
Micah Walter <47419+micahwalter@users.noreply.github.com>
2024-02-15 16:01:45 +0000

    adding support for Amazon Titan

--- providers/amazon.go
+++ providers/amazon.go
@@ -1,13 +1,24 @@
 package providers
 
 type AmazonTitanTextInvokeModelInput struct {
+	Prompt string                          `json:"inputText"`
+	Config AmazonTitanTextGenerationConfig `json:"textGenerationConfig"`
+}
+
+type AmazonTitanTextGenerationConfig struct {
+	Temperature       float64  `json:"temperature"`
+	TopP              float64  `json:"topP"`
+	MaxTokensToSample int      `json:"maxTokenCount"`
+	StopSequences     []string `json:"stopSequences"`
 }
 
 type AmazonTitanTextInvokeModelOutput struct {
+	InputTextTokenCount int                      `json:"inputTextTokenCount"`
+	Results             []AmazonTitanTextResults `json:"results"`
 }
 
-type AmazonTitanEmbeddingsInvokeModelInput struct {
-}
-
-type AmazonTitanEmbeddingsInvokeModelOutput struct {
+type AmazonTitanTextResults struct {
+	TokenCount       int    `json:"tokenCount"`
+	OutputText       string `json:"outputText"`
+	CompletionReason string `json:"completionReason"`
 }
