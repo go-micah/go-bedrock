f531a541d7b6ce097371312d4a44126d0fdb83ba
Micah Walter <47419+micahwalter@users.noreply.github.com>
2024-03-06 22:28:51 -0500

    adding legacy types back

--- providers/anthropic.go
+++ providers/anthropic.go
@@ -1,6 +1,21 @@
 package providers
 
 type AnthropicClaudeInvokeModelInput struct {
+	Prompt            string   `json:"prompt"`
+	MaxTokensToSample int      `json:"max_tokens_to_sample"`
+	Temperature       float64  `json:"temperature"`
+	TopK              int      `json:"top_k"`
+	TopP              float64  `json:"top_p"`
+	StopSequences     []string `json:"stop_sequences"`
+	AnthropicVersion  string   `json:"anthropic_version"`
+}
+
+type AnthropicClaudeInvokeModelOutput struct {
+	Completion string `json:"completion"`
+	StopReason string `json:"stop_reason"`
+}
+
+type AnthropicClaudeMessagesInvokeModelInput struct {
 	System           string                   `json:"system,omitempty"`
 	Messages         []AnthropicClaudeMessage `json:"messages"`
 	MaxTokens        int                      `json:"max_tokens"`
@@ -28,7 +43,7 @@ type AnthropicClaudeSource struct {
 	Data      string `json:"data"`
 }
 
-type AnthropicClaudeInvokeModelOutput struct {
+type AnthropicClaudeInvokeMessagesModelOutput struct {
 	Content    []AnthropicClaudeContent `json:"content"`
 	StopReason string                   `json:"stop_reason"`
 }
